48a310c19004dd12d240d01073b8452c07da5837
chancock1 <chancock@prototest.com>
2013-10-09 18:00:50 -0600

    Finished Menu sub menus

--- src/main/java/com/prototest/MakrScreens/Makr_Menu_User_Help.java
+++ src/main/java/com/prototest/MakrScreens/Makr_Menu_User_Help.java
@@ -18,6 +18,10 @@ public class Makr_Menu_User_Help extends appiumScreenBase {
     appElement Help = new appElement("Makr_Menu_User_Help",By.xpath("//windown[1]/button[4]"));
     appElement FAQs = new appElement("FAQs",By.xpath("//window[1]/button[12]"));
     appElement ContactUs = new appElement("ContactUs",By.xpath("//window[1]button[13]"));
+    //FAQs Sub Screen
+    appElement FirstTopic = new appElement("FirstTopic",By.xpath("//window[1]/button[13]"));
+    appElement SecondTopic = new appElement("SecondTopic",By.xpath("//window{1}/button[14]"));
+    appElement ThirdTopic = new appElement("ThirdTopic",By.xpath("//window[1]/button[15]"));
 
     List<appElement> ScreenElements;
 
@@ -33,7 +37,9 @@ public class Makr_Menu_User_Help extends appiumScreenBase {
         ScreenElements.add(Help);
         ScreenElements.add(FAQs);
         ScreenElements.add(ContactUs);
-
+        ScreenElements.add(FirstTopic);
+        ScreenElements.add(SecondTopic);
+        ScreenElements.add(ThirdTopic);
     }
     public void TapHelp() {
         Help.tap();
@@ -44,5 +50,13 @@ public class Makr_Menu_User_Help extends appiumScreenBase {
     public void TapContactUs() {
         ContactUs.tap();
     }
-
+    public void TapFirstTopic() {
+        FirstTopic.tap();
+    }
+    public void TapSecondTopic() {
+        SecondTopic.tap();
+    }
+    public void TapThirdTopic() {
+        ThirdTopic.tap();
+    }
 }
